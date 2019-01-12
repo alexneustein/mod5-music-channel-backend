b5819150a79ca3e33806caaec4d4b9449cf6fcfa
Alex Neustein <alex.neustein@gmail.com>
2019-01-12 16:11:37 -0500

    updated redis URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.web_socket_server_url = "wss://pianocast-backend.herokuapp.com/cable" 
+
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
