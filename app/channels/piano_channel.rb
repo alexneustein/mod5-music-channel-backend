e36a23cb172f1f726470e5992731899de1be099a
Alex Neustein <alex.neustein@gmail.com>
2019-01-13 19:29:06 -0500

    Create piano_channel.rb

---
+++ app/channels/piano_channel.rb
@@ -0,0 +1,16 @@
+class PianoChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "piano_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def onChat(data)
+    @message = data
+    # @users = self.count_unique_connections
+    # ChatChannel.broadcast_to 'chat_channel', message: @message
+    ActionCable.server.broadcast('piano_channel', message: @message)
+  end
+end
